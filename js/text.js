f29d198c0f154fe59e84000fccfe0f8d9fa32ddc
Parking Master <parkingmaster@email.com>
2026-04-17 19:36:42 -0400

    Finish for today

--- js/text.js
+++ js/text.js
@@ -1,14 +1,17 @@
 text = {
-  createUsername: function(username) {
+  createUsername: function(username, isEnemy) {
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
-    const metrics = ctx.measureText(username);
+    const metrics = ctx.measureText(username.toUpperCase());
     canvas.width = Math.max(metrics.width + 8, 150);
     canvas.height = 96;
-    ctx.fillStyle = "#ff0000";
+    ctx.fillStyle = isEnemy ? "#ff0000" : "#6dc2e7";
     ctx.font = "16px Arial";
+    ctx.fontWeight = "600";
     ctx.textAlign = "center";
-    ctx.fillText(username, canvas.width / 2, canvas.height / 2 - 10);
+    ctx.shadowColor = isEnemy ? "#ff3333" : "#baebff";
+    ctx.shadowBlur = 1;
+    ctx.fillText(username.toUpperCase(), canvas.width / 2, canvas.height / 2 - 10);
     const arrowWidth = 10;
     const arrowHeight = 10;
     const arrowStart = [canvas.width / 2, canvas.height / 2 + 16 - 10];
@@ -19,9 +22,10 @@ text = {
     ctx.fill();
     const texture = new THREE.Texture(canvas);
     texture.needsUpdate = true;
-    const sprite = new THREE.Sprite(new THREE.SpriteMaterial({ map: texture, sizeAttenuation: false }));
-    sprite.scale.set(0.3, 0.25, 0.25);
-    sprite.position.y = 1.5;
+    const sprite = new THREE.Sprite(new THREE.SpriteMaterial({ map: texture, sizeAttenuation: false, depthTest: isEnemy, depthWrite: false }));
+    sprite.scale.set(0.15, 0.09, 1);
+    sprite.position.y = 1.3;
+    sprite.name = "UsernamePointer";
     return sprite;
   }
 };
