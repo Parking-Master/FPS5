cc736e307783a238384490c220a1f89d21c565ab
Parking Master <parkingmaster@email.com>
2026-08-19 21:29:07 -0400

    Finish for today

--- js/audio.js
+++ js/audio.js
@@ -16,7 +16,12 @@ const AudioWrapper = function(src, alias = false) {
       startTime: 0,
       offset: 0,
       playing: false,
-      loadPromise: fetch(src).then(res => res.arrayBuffer()).then(buf => ctx.decodeAudioData(buf)).then(decoded => wrapper.buffer = decoded),
+      loaded: false,
+      loadPromise: fetch(src).then(res => res.arrayBuffer()).then(buf => ctx.decodeAudioData(buf)).then(decoded => {
+        wrapper.buffer = decoded;
+        wrapper.loaded = true;
+        audio.checkIfLoaded();
+      }),
       play: function() {
         if (!wrapper.buffer) {
           wrapper.loadPromise.then(() => wrapper.play());
@@ -69,7 +74,9 @@ const AudioWrapper = function(src, alias = false) {
 };
 
 const AudioWrapper3d = function(src, alias = false) {
-  let mainWrapper = {};
+  let mainWrapper = {
+    loaded: false
+  };
   new THREE.AudioLoader().load(src, buffer => {
     let steps = 1;
     if (alias) steps = 2;
@@ -79,9 +86,10 @@ const AudioWrapper3d = function(src, alias = false) {
       scene.add(sound);
       if (i == 0) {
         mainWrapper.sound = sound;
+        mainWrapper.loaded = true;
+        audio.checkIfLoaded();
       } else {
         mainWrapper.sound.alias = sound;
-
       }
     }
   });
@@ -92,10 +100,62 @@ let currentExplodeSound = 0;
 
 audio = {
   ctx: new (window.AudioContext || window.webkitAudioContext)(),
-  initialize: function() {
+  finishCallback: null,
+  checkIfLoaded: function() {
+    if (Object.values(audio.sounds).filter(a => a.loaded).length == Object.values(audio.sounds).length) audio.finishCallback();
+  },
+  initialize: function(callback) {
     const listener = new THREE.AudioListener();
     scene.add(listener);
     camera.audioListener = listener;
+    let weapons = Object.keys(sandbox.weapons);
+    audio.finishCallback = function() {
+      callback();
+    };
+    audio.sounds = {
+      "map.ambience.0": AudioWrapper("/sounds/ambience/0.mp3"),
+      "grenade.hit.0": AudioWrapper3d("/sounds/grenades/hits/0.mp3"),
+      "grenade.explosion.mk2": AudioWrapper3d("/sounds/grenades/explosions/0.mp3", true),
+      "grenade.explosion.plasma.0": AudioWrapper3d("/sounds/grenades/explosions/1.mp3"),
+      "grenade.explosion.plasma.1": AudioWrapper3d("/sounds/grenades/explosions/2.mp3"),
+      "grenade.explosion.plasma.2": AudioWrapper3d("/sounds/grenades/explosions/3.mp3"),
+      "ricochet.0": AudioWrapper3d("/sounds/weapons/ricochet/0.mp3"),
+      "ricochet.1": AudioWrapper3d("/sounds/weapons/ricochet/1.mp3"),
+      "ricochet.2": AudioWrapper3d("/sounds/weapons/ricochet/2.mp3"),
+      "shields.low": AudioWrapper("/sounds/shields/low.mp3"),
+      "shields.warning": AudioWrapper("/sounds/shields/warning.mp3"),
+      "dying.0": AudioWrapper3d("/sounds/dying/0.mp3"),
+      "hit.headshot": AudioWrapper("/sounds/shields/hit-headshot.mp3"),
+      "hit": AudioWrapper("/sounds/shields/hit.mp3")
+    };
+    for (let i = 0; i < weapons.length; i++) {
+      if (sandbox.weapons[weapons[i]].loaded) {
+        audio.sounds["reload." + weapons[i] + ".normal"] = AudioWrapper("/sounds/weapons/reloading/" + weapons[i] + ".mp3");
+        audio.sounds["reload." + weapons[i] + ".full"] = AudioWrapper("/sounds/weapons/reloading/" + weapons[i] + ".full.mp3");
+        audio.sounds["fire." + weapons[i]] = AudioWrapper("/sounds/weapons/shooting/" + weapons[i] + ".mp3", true);
+      }
+      audio.sounds["3d.fire." + weapons[i]] = AudioWrapper3d("/sounds/weapons/shooting/" + weapons[i] + ".mp3", true);
+    }
+    if (sandbox.maps[sandbox.presets.map].vehicles.filter(vehicle => vehicle.type == "Warthog").length > 0) {
+      audio.sounds["vehicle.Warthog.accelerate"] = AudioWrapper3d("/sounds/vehicles/Jeep.accelerate.mp3", true);
+      audio.sounds["vehicle.Warthog.decelerate"] = AudioWrapper3d("/sounds/vehicles/Jeep.decelerate.mp3", true);
+      audio.sounds["vehicle.Warthog.idle"] = AudioWrapper3d("/sounds/vehicles/Jeep.idle.mp3", true);
+      audio.sounds["vehicle.Warthog.major-crash"] = AudioWrapper3d("/sounds/vehicles/Jeep.major-crash.mp3");
+      audio.sounds["vehicle.Warthog.big-crash"] = AudioWrapper3d("/sounds/vehicles/Jeep.big-crash.mp3");
+      audio.sounds["vehicle.Warthog.small-crash"] = AudioWrapper3d("/sounds/vehicles/Jeep.small-crash.mp3");
+      audio.sounds["vehicle.Warthog.explosion"] = AudioWrapper3d("/sounds/vehicles/Jeep.explosion.mp3");
+      audio.sounds["vehicle.Warthog.horn"] = AudioWrapper3d("/sounds/vehicles/Jeep.horn.mp3", true);
+    }
+    if (sandbox.maps[sandbox.presets.map].vehicles.filter(vehicle => vehicle.type == "Jeep").length > 0) {
+      audio.sounds["vehicle.Jeep.accelerate"] = AudioWrapper3d("/sounds/vehicles/Jeep.accelerate.mp3", true);
+      audio.sounds["vehicle.Jeep.decelerate"] = AudioWrapper3d("/sounds/vehicles/Jeep.decelerate.mp3", true);
+      audio.sounds["vehicle.Jeep.idle"] = AudioWrapper3d("/sounds/vehicles/Jeep.idle.mp3", true);
+      audio.sounds["vehicle.Jeep.major-crash"] = AudioWrapper3d("/sounds/vehicles/Jeep.major-crash.mp3");
+      audio.sounds["vehicle.Jeep.big-crash"] = AudioWrapper3d("/sounds/vehicles/Jeep.big-crash.mp3");
+      audio.sounds["vehicle.Jeep.small-crash"] = AudioWrapper3d("/sounds/vehicles/Jeep.small-crash.mp3");
+      audio.sounds["vehicle.Jeep.explosion"] = AudioWrapper3d("/sounds/vehicles/Jeep.explosion.mp3");
+      audio.sounds["vehicle.Jeep.horn"] = AudioWrapper3d("/sounds/vehicles/Jeep.horn.mp3", true);
+    }
   },
   update: function(cameraPosition, cameraRotation) {
     if (typeof camera.audioListener != "undefined") {
@@ -313,54 +373,6 @@ audio = {
     sound.play();
   }
 };
-
-audio.sounds = {
-  "reload.AK-74.normal": AudioWrapper("/sounds/weapons/reloading/AK-74.mp3"),
-  "reload.AK-74.full": AudioWrapper("/sounds/weapons/reloading/AK-74.full.mp3"),
-  "fire.AK-74": AudioWrapper("/sounds/weapons/shooting/AK-74.mp3", true),
-  "reload.FN-502.normal": AudioWrapper("/sounds/weapons/reloading/FN-502.mp3"),
-  "reload.FN-502.full": AudioWrapper("/sounds/weapons/reloading/FN-502.full.mp3"),
-  "fire.FN-502": AudioWrapper("/sounds/weapons/shooting/FN-502.mp3"),
-  "reload.SCAR-H.normal": AudioWrapper("/sounds/weapons/reloading/SCAR-H.mp3"),
-  "reload.SCAR-H.full": AudioWrapper("/sounds/weapons/reloading/SCAR-H.full.mp3"),
-  "fire.SCAR-H": AudioWrapper("/sounds/weapons/shooting/SCAR-H.mp3", true),
-  "reload.30-SST.normal": AudioWrapper("/sounds/weapons/reloading/30-SST.mp3"),
-  "reload.30-SST.full": AudioWrapper("/sounds/weapons/reloading/30-SST.full.mp3"),
-  "fire.30-SST": AudioWrapper("/sounds/weapons/shooting/30-SST.mp3", true),
-  "reload.HK-G28.normal": AudioWrapper("/sounds/weapons/reloading/HK-G28.mp3"),
-  "reload.HK-G28.full": AudioWrapper("/sounds/weapons/reloading/HK-G28.full.mp3"),
-  "fire.HK-G28": AudioWrapper("/sounds/weapons/shooting/HK-G28.mp3"),
-  "reload.Ace-35r.normal": AudioWrapper("/sounds/weapons/reloading/Ace-35r.mp3"),
-  "reload.Ace-35r.full": AudioWrapper("/sounds/weapons/reloading/Ace-35r.full.mp3"),
-  "fire.Ace-35r": AudioWrapper("/sounds/weapons/shooting/Ace-35r.mp3", true),
-  "grenade.hit.0": AudioWrapper3d("/sounds/grenades/hits/0.mp3"),
-  "grenade.explosion.mk2": AudioWrapper3d("/sounds/grenades/explosions/0.mp3", true),
-  "grenade.explosion.plasma.0": AudioWrapper3d("/sounds/grenades/explosions/1.mp3"),
-  "grenade.explosion.plasma.1": AudioWrapper3d("/sounds/grenades/explosions/2.mp3"),
-  "grenade.explosion.plasma.2": AudioWrapper3d("/sounds/grenades/explosions/3.mp3"),
-  "map.ambience.0": AudioWrapper("/sounds/ambience/0.mp3"),
-  "dying.0": AudioWrapper3d("/sounds/dying/0.mp3"),
-  "vehicle.Jeep.accelerate": AudioWrapper3d("/sounds/vehicles/Jeep.accelerate.mp3", true),
-  "vehicle.Jeep.decelerate": AudioWrapper3d("/sounds/vehicles/Jeep.decelerate.mp3", true),
-  "vehicle.Jeep.idle": AudioWrapper3d("/sounds/vehicles/Jeep.idle.mp3", true),
-  "vehicle.Jeep.major-crash": AudioWrapper3d("/sounds/vehicles/Jeep.major-crash.mp3"),
-  "vehicle.Jeep.big-crash": AudioWrapper3d("/sounds/vehicles/Jeep.big-crash.mp3"),
-  "vehicle.Jeep.small-crash": AudioWrapper3d("/sounds/vehicles/Jeep.small-crash.mp3"),
-  "vehicle.Jeep.explosion": AudioWrapper3d("/sounds/vehicles/Jeep.explosion.mp3"),
-  "vehicle.Jeep.horn": AudioWrapper3d("/sounds/vehicles/Jeep.horn.mp3", true),
-  "ricochet.0": AudioWrapper3d("/sounds/weapons/ricochet/0.mp3"),
-  "ricochet.1": AudioWrapper3d("/sounds/weapons/ricochet/1.mp3"),
-  "ricochet.2": AudioWrapper3d("/sounds/weapons/ricochet/2.mp3"),
-  "shields.low": AudioWrapper("/sounds/shields/low.mp3"),
-  "shields.warning": AudioWrapper("/sounds/shields/warning.mp3"),
-  "hit.headshot": AudioWrapper("/sounds/shields/hit-headshot.mp3"),
-  "hit": AudioWrapper("/sounds/shields/hit.mp3")
-};
-for (sound in audio.sounds) {
-  if (sound.startsWith("fire.")) {
-    audio.sounds["3d." + sound] = AudioWrapper3d(audio.sounds[sound].src, true);
-  }
-}
 
 document.addEventListener("touchstart", () => {
   if (audio.ctx.state === "suspended") audio.ctx.resume();
